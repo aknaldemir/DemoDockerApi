ebe0fc4987ad2a1a5bc195237ce363efaa6aabe1
Akın ALDEMİR <aknaldemir@gmail.com>
2022-03-27 12:31:32 +0300

    .

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -22,10 +22,12 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+
+        int nmber= 55;
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
+            TemperatureC = Random.Shared.Next(-20, nmber),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
